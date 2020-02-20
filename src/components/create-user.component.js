f5c1ba8d519d41fbd5bd436fa52abb005d7fccdf
sωıтcн <xspynni23@gmail.com>
2020-02-20 10:19:12 -0300

    components update

---
+++ src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateUsers extends Component {
+    render() {
+        return(
+            <div>
+                <p>aqui fica CreateUsers</p>
+            </div>
+        )
+    }
+}
